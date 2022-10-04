a1c5aadafd49691c62d5433eb6b2aa2bd8660f6f
TomPadmanathan <padmanathantom@gmail.com>
2022-10-04 12:41:13 +0100

    Added error checking for inputs over 4 characters continuous

--- main.js
+++ main.js
@@ -16,10 +16,18 @@ function submit() {
 
     // Adds up numArray
     for(let i = 0; i < numArray.length; i++) {
-        if(numArray[i] < numArray[i + 1]) {
+        if(numArray[i] + numArray[i + 1] + numArray[i + 2] + numArray[i + 3] == numArray[i] * 4) {
+            error('Invalid input')
+            document.getElementById('output').value = null
+            return
+        } else if(numArray[i] < numArray[i + 1]) {
             output += numArray[i + 1] - numArray[i]
             i += 1
-        } else output += numArray[i]
+            error(null)
+        } else  {
+            output += numArray[i]
+            error(null)
+        }
     }
 
     if(input == '') document.getElementById('output').value = null
@@ -37,3 +45,62 @@ function copy() {
 		document.getElementById('output').value
 	)
 }
+
+
+function error(error) {
+	document.getElementById('error').innerHTML = error;
+}
+
+
+
+
+
+
+
+
+
+
+// const values = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000
+// }
+// function submit() {
+//     const input = document.getElementById('input').value
+//     let numArray = [], output = 0, romanArray = input.toUpperCase().split('')
+
+//     // Converts roman numerials to ints
+//     for(let i = 0; i < romanArray.length; i++) numArray.push(values[romanArray[i]])
+
+//     // Adds up numArray
+//     for(let i = 0; i < numArray.length; i++) {
+//         if(numArray[i] < numArray[i + 1]) {
+//             output += numArray[i + 1] - numArray[i]
+//             i += 1
+//         } else output += numArray[i]
+//     }
+
+//     if(input == '') document.getElementById('output').value = null
+//     else {
+//         document.getElementById('output').value = output
+//         document.getElementById('input').value = null
+//     }
+//     console.log("Output: " + output)
+//     console.log("Roman Numerals: " + romanArray)
+//     console.log("Numbers: " + numArray)
+// }
+
+// function copy() {
+//     navigator.clipboard.writeText(
+// 		document.getElementById('output').value
+// 	)
+// }
+
+
+// function error(error) {
+// 	document.getElementById('error').innerHTML = error;
+// }
